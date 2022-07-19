884342267df96df98d70ad72f371e3d3fb0a435c
DA_M1_64 <wandilemaluleke@gmail.com>
2022-07-19 10:13:43 +0200

    new pages added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import * as React from "react";
+import {Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 import './App.css';
+import List from "./pages/List/List";
+import Hotel from "./pages/home/hotel/Hotel";
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/id" element={<Hotel />} /> 
+      </Routes>
     </div>
   );
 }
