77b59d686abb991e2253b7228f4b8625aa6b273c
DA_M1_64 <wandilemaluleke@gmail.com>
2022-07-20 12:56:08 +0200

    header placeholder added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,42 +1,63 @@
-// import { faBed } from "@fortawesome/free-regular-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBed , faPlane, faCar, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { faBed, faPlane, faCar, faTaxi, faCalendarDay, faPerson } from '@fortawesome/free-solid-svg-icons'
 import "./header.css"
 
 const Header = () => {
     return (
         <div className="header">
-            <div className="headerList">
-                <div className="headerListItem">
-                <FontAwesomeIcon icon={faBed} />
-                <span>Stays</span>
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem active">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Stays</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faPlane} />
+                        <span>Flights</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faCar} />
+                        <span>Car rentals</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Attractions</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faTaxi} />
+                        <span>Airport taxis</span>
+                    </div>
                 </div>
-            </div>
-            <div className="headerList">
-                <div className="headerListItem">
-                <FontAwesomeIcon icon={faPlane} />
-                <span>Flights</span>
-                </div>
-            </div>
-            <div className="headerList">
-                <div className="headerListItem">
-                <FontAwesomeIcon icon={faCar} />
-                <span>Car rentals</span>
-                </div>
-            </div>
-            <div className="headerList">
-                <div className="headerListItem">
-                <FontAwesomeIcon icon={faBed} />
-                <span>Attractions</span>
-                </div>
-            </div>
-            <div className="headerList">
-                <div className="headerListItem">
-                <FontAwesomeIcon icon={faTaxi} />
-                <span>Airport taxis</span>
+                <h1 className="headerTitlle">A Lifetime of discounts? It's Genuis.</h1>
+                <p className="headerDesc">
+                    Get reward for your travels - unlocked instant savings of 10% or more
+                    with a free Lamaboooking acccount
+                </p>
+                <button className="headerBtn">Sign in / Register</button>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faBed} className="headerIcon" />
+                      <input 
+                      type="text" 
+                      placeholder="where are you going?" 
+                      className="headerSearchInput" 
+                      />
+                    </div>
+                    <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faCalendarDay} className="headerIcon" />
+                      <span className="headerSearchText">date to date</span>
+                    </div>
+                    <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+                    <span className="headerSearchText">2 adults 2 childen 1 room</span>
+                    </div>
+                    <div className="headerSearchItem">
+                    <button className="headerBtn">Search</button>
+                    </div>
                 </div>
             </div>
         </div>
+        
     )
 }
 
