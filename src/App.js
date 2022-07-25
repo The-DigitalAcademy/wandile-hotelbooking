3ccfc28351e713e034c5ba7e9ad59a29b5c883ae
DA_M1_64 <wandilemaluleke@gmail.com>
2022-07-25 16:06:16 +0200

    Navigate added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Hotel from "./pages/hotel/Hotel";
@@ -13,7 +13,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<List />} />
         <Route path="/hotels/id" element={<Hotel />} /> 
-      </Routes>
+        <Route
+        path="*"
+        element={<Navigate to="/" replace />}
+      />
+        </Routes>
       </BrowserRouter>
     </div>
   );
