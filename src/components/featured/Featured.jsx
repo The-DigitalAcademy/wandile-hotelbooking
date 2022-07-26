dd987d58ec12c0bb3dc98d95608162e76bc2945b
DA_M1_64 <wandilemaluleke@gmail.com>
2022-07-26 12:14:19 +0200

    new 3 pictures in Featured added

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,35 +5,35 @@ const Featured = () => {
     <div className="featured">
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          src="https://www.andbeyond.com/wp-content/uploads/sites/5/Johannesburg-Skyline.jpg"
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Dublin</h1>
+          <h1>Johannesburg</h1>
           <h2>123 properties</h2>
         </div>
       </div>
       
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+          src="https://static.trip101.com/paragraph_media/pictures/001/820/085/large/Durban_beach_front__281_of_1_29.jpg?1583409245"
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Reno</h1>
+          <h1>Durban</h1>
           <h2>533 properties</h2>
         </div>
       </div>
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+          src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/14/10/2e/1e/cape-town.jpg?w=700&h=-1&s=1"
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Austin</h1>
+          <h1>Cape Town</h1>
           <h2>532 properties</h2>
         </div>
       </div>
