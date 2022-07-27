165a5f703bbaf4eb827b753223704755de0667fc
DA_M1_64 <wandilemaluleke@gmail.com>
2022-07-27 11:00:58 +0200

    makes pictures of FeaturedProperties added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Featured from "../../components/featured/Featured";
 import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.css";
@@ -13,6 +14,8 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList/>
+        <h1 className="homeTitle">Home guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   );
