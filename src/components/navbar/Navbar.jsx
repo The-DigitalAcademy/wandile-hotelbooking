fc5982485217959e31527a2412ebea7593fd8f71
DA_M1_64 <wandilemaluleke@gmail.com>
2022-08-10 10:11:44 +0200

    change a navbar and footer addded

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import "./navbar.css"
+// import { useRouter } from "react-router-dom";
 
 const Navbar = () => {
+
+    // let router = useRouter();
+
+    // const goTo = (path) => {
+    //     history.pushState(path);
+    //     window.location.reload();
+    // };
+
     return (
         <div className="navbar">
             <div className="navContainer">
                 <span className="logo">iambooking</span>
                 <div className="navItems">
                 {/* <button className="navButton">Register</button> */}
-                <button className="navButton">Login / Sign in</button>
+                <button  className="navButton">Login / Sign in</button>
                 </div>
             </div>
         </div>
