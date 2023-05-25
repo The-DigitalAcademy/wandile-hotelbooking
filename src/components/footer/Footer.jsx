6a5013790874c04f3d8ceb8b10ac94aab7044a6d
wandileo <wandilemaluleke@gmail.com>
2023-05-25 13:21:58 +0200

    change a date of year

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2022 Digital Academy - Wandile Maluleke. All Rights Reserved</div>
+      <div className="fText">Copyright © 2023 Digital Academy - Wandile Maluleke. All Rights Reserved</div>
     </div>
   );
 };
